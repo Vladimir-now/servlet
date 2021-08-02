182c96b4af06e193a5b9e1682b6bf961e81e969c
vladimir_vasilyev <fargus9595@gmail.com>
2021-08-02 12:37:15 +0300

    1.1 fix endpoint put

--- src/main/java/ru/appline/ServletPut.java
+++ src/main/java/ru/appline/ServletPut.java
@@ -6,12 +6,13 @@ import com.google.gson.JsonObject;
 import ru.appline.logic.Model;
 import ru.appline.logic.User;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Map;
 
 import static ru.appline.common.Common.readRequest;
 
@@ -19,10 +20,9 @@ import static ru.appline.common.Common.readRequest;
 public class ServletPut extends HttpServlet {
 
     private final Model model = Model.getInstance();
-    private final AtomicInteger counter = new AtomicInteger(model.getFromList().size() + 1);
     private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json;charset=utf-8");
         request.setCharacterEncoding("UTF-8");
 
@@ -31,6 +31,12 @@ public class ServletPut extends HttpServlet {
         PrintWriter pw = response.getWriter();
 
         int id = jobj.get("id").getAsInt();
+
+        if(id <= 0) {
+            pw.print(gson.toJson("ID пользователя должно быть больше 0!"));
+            return;
+        }
+
         String name = jobj.get("name").getAsString();
         String surname = jobj.get("surname").getAsString();
         double salary = jobj.get("salary").getAsDouble();
@@ -38,8 +44,23 @@ public class ServletPut extends HttpServlet {
         User user = new User(name, surname, salary);
 
         if (!model.getFromList().containsKey(id)) {
-            model.add(user, id);
-            pw.print(gson.toJson("Был создан новый пользователь! ID пользователя: " + id));
+            boolean isCreate = false;
+            Integer i = 1;
+            for (Map.Entry u: model.getFromList().entrySet()) {
+                if (!i.equals(u.getKey())) {
+                    model.add(user, i);
+                    isCreate = true;
+                    pw.print(gson.toJson("Был создан новый пользователь! ID пользователя: " + i));
+                    break;
+                } else {
+                    i++;
+                }
+            }
+            if (!isCreate) {
+                model.add(user, i);
+                pw.print(gson.toJson("Был создан новый пользователь! ID пользователя: " + i));
+            }
+
         } else {
             model.add(user, id);
             pw.print(gson.toJson("Пользователь с ID: " + id + " был изменен!"));
