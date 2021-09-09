9eab30a5b49edcf9146c06bbaa6fcae3b3778980
vladimir_vasilyev <fargus9595@gmail.com>
2021-09-09 19:19:48 +0300

    fix name

--- src/main/java/ru/appline/logic/Model.java
+++ src/main/java/ru/appline/logic/Model.java
@@ -3,7 +3,6 @@ package ru.appline.logic;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Model implements Serializable {
 
@@ -20,7 +19,7 @@ public class Model implements Serializable {
         model.put(1, new User("Tigran", "Mantashyan", 55555));
         model.put(2, new User("Anton", "Osipov", 66666));
         model.put(3, new User("Zoya", "Andreeva", 77777));
-        model.put(4, new User("Elensa", "Vorobey", 88888));
+        model.put(4, new User("Elena", "Vorobey", 88888));
     }
 
     public void add(User user, int id) {
